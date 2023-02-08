d331a85389838c49443a2f7c30c4b4bef2617f73
Ryan Howard <ryanghoward@protonmail.com>
2023-02-07 16:32:12 -0800

    Link Input

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Youtube Downloader")
 
+# UI Elements
+title = customtkinter.CTkLabel(app, text='Insert a YouTube link')
+title.pack(padx=10, pady=10)
+
+# Link input
+link = customtkinter.CTkEntry(app, width=350, height=40)
+link.pack()
+
 # Run app
 app.mainloop()
