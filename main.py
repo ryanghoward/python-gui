974e4409b549df250c2369a0845d0a66cd96b928
Ryan Howard <ryanghoward@protonmail.com>
2023-02-07 17:12:17 -0800

    Progress bar

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ from pytube import YouTube
 def startDownload(): 
   try: 
     ytLink = link.get()
-    ytObject = YouTube(ytLink)
+    ytObject = YouTube(ytLink, on_progress_callback = on_progress)
     video = ytObject.streams.get_highest_resolution()
 
     title.configure(text = ytObject.title, text_color = 'white')
     finishLabel.configure(text = '')
     video.download()
+    finishLabel.configure(text = 'Video Successfully Downloaded :)')
   except:
     finishLabel.configure(text = 'Download Error :(', text_color = 'red')
-  finishLabel.configure(text = 'Video Successfully Downloaded :)')
 
+def on_progress(stream, chunk, bytes_remaining):
+  total_size = stream.filesize
+  bytes_downloaded = total_size - bytes_remaining
+  percentage_of_completion = bytes_downloaded / total_size * 100
+  per = str(int(percentage_of_completion))
+  pPercentage.configure(text = per + '%')
+  pPercentage.update()
+
+  # Update Progress Bar
+  progressBar.set(float(percentage_of_completion) / 100)
 
 # System Settings
 customtkinter.set_appearance_mode("System")
@@ -37,6 +47,14 @@ link.pack()
 # Finished Downloading
 finishLabel = customtkinter.CTkLabel(app, text = '')
 finishLabel.pack()
+
+# Progress percentage
+pPercentage = customtkinter.CTkLabel(app, text = '0%')
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width = 400)
+progressBar.set(0)
+progressBar.pack(padx = 20, pady = 20)
 
 # Download Button
 download = customtkinter.CTkButton(app, text = 'Download', command = startDownload)
