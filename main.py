4577b2f8428b9858f12a3390511bafa588c78fdf
Ryan Howard <ryanghoward@protonmail.com>
2023-02-07 16:57:22 -0800

    Video title appears upon successful download

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ def startDownload():
     ytLink = link.get()
     ytObject = YouTube(ytLink)
     video = ytObject.streams.get_highest_resolution()
+
+    title.configure(text = ytObject.title, text_color = 'white')
+    finishLabel.configure(text = '')
     video.download()
   except:
-    finishLabel.configure(text='Download Error :(')
-  finishLabel.configure(text='Video Successfully Downloaded :)')
+    finishLabel.configure(text = 'Download Error :(', text_color = 'red')
+  finishLabel.configure(text = 'Video Successfully Downloaded :)')
 
 
 # System Settings
@@ -23,21 +26,21 @@ app.geometry("720x480")
 app.title("Youtube Downloader")
 
 # UI Elements
-title = customtkinter.CTkLabel(app, text='Insert a YouTube link')
-title.pack(padx=10, pady=10)
+title = customtkinter.CTkLabel(app, text = 'Insert a YouTube link')
+title.pack(padx = 10, pady = 10)
 
 # Link input
 url_var = tkinter.StringVar()
-link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link = customtkinter.CTkEntry(app, width = 350, height = 40, textvariable = url_var)
 link.pack()
 
 # Finished Downloading
-finishLabel = customtkinter.CTkLabel(app, text='')
+finishLabel = customtkinter.CTkLabel(app, text = '')
 finishLabel.pack()
 
 # Download Button
-download = customtkinter.CTkButton(app, text='Download', command=startDownload)
-download.pack(padx=20, pady=20)
+download = customtkinter.CTkButton(app, text = 'Download', command = startDownload)
+download.pack(padx = 20, pady = 20)
 
 # Run app
 app.mainloop()
